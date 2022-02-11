e98f89aa66517911fadc116ee3bbf766dd656d70
Frank Fang <frankfr@email.unc.edu>
2022-02-10 21:48:11 -0500

    updated again

--- flips.js
+++ flips.js
@@ -4,13 +4,14 @@ import { createRequire } from 'module';
 
 if(process.argv.length < 3){
     console.log("Error: no input.\nUsage: node flips.js --number=#ofFlips");
+} else {
+    const require = createRequire(import.meta.url);
+    const argv = require('minimist')(process.argv.slice(2));
+
+    const number = argv.number
+
+    // console.log(number);
+    var flips = coinFlips(number)
+    console.log(flips);
+    console.log(countFlips(flips));
 }
-const require = createRequire(import.meta.url);
-const argv = require('minimist')(process.argv.slice(2));
-
-const number = argv.number
-
-// console.log(number);
-var flips = coinFlips(number)
-console.log(flips);
-console.log(countFlips(flips));
