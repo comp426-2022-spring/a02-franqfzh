8b1418a2c07917f30563acfeb5b6afe5f793d6bf
Frank Fang <frankfr@email.unc.edu>
2022-02-10 23:28:26 -0500

    d

--- flips.js
+++ flips.js
@@ -8,7 +8,6 @@ const require = createRequire(import.meta.url);
 const argv = require('minimist')(process.argv.slice(2));
 const number = argv.number
 if (Number.isInteger(number)){
-    console.log(number)
     var flips = coinFlips(number)
     console.log(flips);
     console.log(countFlips(flips));
