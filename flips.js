7843852a13bdbb397311df9cc0a1b259cc405b42
Frank Fang <frankfr@email.unc.edu>
2022-02-10 21:54:36 -0500

    throw error

--- flips.js
+++ flips.js
@@ -3,15 +3,16 @@ import { countFlips } from "./modules/coin.mjs";
 import { createRequire } from 'module';
 
 if(process.argv.length < 3){
-    console.log("Error: no input.\nUsage: node flips.js --number=#ofFlips");
-} else {
-    const require = createRequire(import.meta.url);
-    const argv = require('minimist')(process.argv.slice(2));
-
-    const number = argv.number
-
-    // console.log(number);
-    var flips = coinFlips(number)
-    console.log(flips);
-    console.log(countFlips(flips));
+    throw "Error: No iput";
 }
+
+const require = createRequire(import.meta.url);
+const argv = require('minimist')(process.argv.slice(2));
+
+const number = argv.number
+
+// console.log(number);
+var flips = coinFlips(number)
+console.log(flips);
+console.log(countFlips(flips));
+
