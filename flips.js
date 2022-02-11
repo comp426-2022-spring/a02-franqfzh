f618c4d5dcd7b491212d2697348973d6dad16b46
Frank Fang <frankfr@email.unc.edu>
2022-02-11 00:07:44 -0500

    sad

--- flips.js
+++ flips.js
@@ -7,16 +7,29 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const argv = require('minimist')(process.argv.slice(2));
 const number = argv.number
-if (Number.isInteger(number)){
-    var flips = coinFlips(number)
-    console.log(flips);
-    console.log(countFlips(flips));
+// if (Number.isInteger(number)){
+//     var flips = coinFlips(number)
+//     console.log(flips);
+//     console.log(countFlips(flips));
     
-} else {
+// } else {
+//     var oneFlip = coinFlips(1);
+//     if (oneFlip == 'tails') {
+//         console.log({tails: 1});
+//     } else {
+//         console.log({heads: 1});
+//     }
+// }
+
+if (isNaN(number)) {
     var oneFlip = coinFlips(1);
     if (oneFlip == 'tails') {
         console.log({tails: 1});
     } else {
         console.log({heads: 1});
     }
+} else {
+    var flips = coinFlips(number)
+    console.log(flips);
+    console.log(countFlips(flips));
 }
